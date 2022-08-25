78c30fea57d1af70418aea49c3eb4ab1bc42aebf
Lee Jaeha <108874515+jaehafe@users.noreply.github.com>
2022-08-25 18:11:19 +0900

    form-input

---
+++ 4-forms/setup/app.js
@@ -0,0 +1,12 @@
+function countBiggerThanTen(numbers) {
+  return numbers.reduce((acc, current) => {
+    if (current > 10) {
+      return acc + 1;
+    } else {
+      return acc;
+    }
+  }, 0);
+}
+
+const count = countBiggerThanTen([1, 2, 3, 5, 10, 20, 30, 40, 50, 60]);
+console.log(count);
