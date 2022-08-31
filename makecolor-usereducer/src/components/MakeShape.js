57e2f43b23efceca046b0da389c0d783001986e4
Lee Jaeha <108874515+jaehafe@users.noreply.github.com>
2022-08-31 15:47:13 +0900

    making color using useReducer  // 주석 추가

--- makecolor-usereducer/src/components/MakeShape.js
+++ makecolor-usereducer/src/components/MakeShape.js
@@ -6,10 +6,14 @@ export const ACTION = {
   DELETE_BOX: 'delete-box',
   SWITCH_BOX: 'switch-box',
 };
-// action은 dispatch에 항상 따라오는 object
+// 1.
+// 4. dispatch 함수를 콜 함과 동시에 reducer 함수가 콜이 됨
 const reducer = (boxes, action) => {
   switch (action.type) {
     case ACTION.ADD_BOX:
+      // 5. ...boxes: previous, 즉 업데이트 전의 상태를 깔아놓고, 새로운 object(newBox)를 가져다 붙임
+      // dispatch 함수 안에서의 payload의 object가 여기서 pickup됨
+      // action은 dispatch에 항상 따라오는 object
       return [...boxes, newBox(action.payload.color)];
     case ACTION.DELETE_BOX:
       return boxes.filter((box) => box.id !== action.payload.id);
@@ -30,15 +34,19 @@ const newBox = (color) => {
 };
 
 const MakeShape = () => {
+  // 1.
   // boxes: 관리하고 싶은 state; dispatch 함수
-  // []: 기본값; reducer: 함수
-  // dispatch 함수를 콜 할 때, 항상 reducer 함수도 콜이 됨
-  // []: 초기상태
+  // []: 기본값, 초기상태; reducer: 함수
+  // 3. dispatch 함수를 콜 할 때, 항상 reducer 함수도 콜이 됨
+
+  // reducer 함수는 현재 상태(state) 객체와 행동(action) 객체를 인자로 받아서 새로운 상태(state) 객체를 반환하는 함수
+  // dispatch 함수는 컴포넌트 내에서 상태 변경을 일으키기 위해서 사용되는데 인자로 reducer 함수에 넘길 행동(action) 객체를 받음
   const [boxes, dispatch] = useReducer(reducer, []);
   const [color, setColor] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // 2
     // dispatch: type 무슨 종류의 액션인지
     // payload: 추가 옵션; color를 따로 픽업해서 가져옴
     dispatch({ type: ACTION.ADD_BOX, payload: { color: color } });
