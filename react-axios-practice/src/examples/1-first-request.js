592eedb34bc83b3fdd247f738b2e559f7710083d
Lee Jaeha <108874515+jaehafe@users.noreply.github.com>
2022-09-02 00:30:46 +0900

    axios - React

---
+++ react-axios-practice/src/examples/1-first-request.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const url = 'https://course-api.com/react-store-productss';
+
+const FirstRequest = () => {
+  const fetchData = async () => {
+    try {
+      const response = await axios(url);
+      const data = await response.data;
+      console.log('data', data);
+    } catch (error) {
+      console.log('error', error.response);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <h2 className="text-center">first request</h2>;
+};
+export default FirstRequest;
