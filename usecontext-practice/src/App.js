4789d2dd10f6133127fd30388bd22d3258537996
Lee Jaeha <108874515+jaehafe@users.noreply.github.com>
2022-09-15 22:23:46 +0900

    useContext Practice

--- usecontext-practice/src/App.js
+++ usecontext-practice/src/App.js
@@ -1,10 +1,11 @@
+import SigninProvider from './ContextProvider';
 import Page from './Page';
 
 const App = () => {
   return (
-    <>
+    <SigninProvider>
       <Page />
-    </>
+    </SigninProvider>
   );
 };
 
--- usecontext-practice/src/Page.js
+++ usecontext-practice/src/Page.js
@@ -1,4 +1,3 @@
-import SigninProvider from './ContextProvider';
 import Card1 from './Card1';
 import Card2 from './Card2';
 import Card3 from './Card3';
@@ -6,11 +5,11 @@ import Card3 from './Card3';
 const Page = () => {
   return (
     // children을 사용할 경우 self closing tag x
-    <SigninProvider>
+    <>
       <Card1 />
       <Card2 />
       <Card3 />
-    </SigninProvider>
+    </>
   );
 };
 
